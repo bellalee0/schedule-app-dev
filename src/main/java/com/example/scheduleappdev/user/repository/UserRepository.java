3efaa73b6aee6fade5bd86e346f226a4d582c346
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-11-11 19:44:32 +0900

    comment: 메서드 주석 추가

--- src/main/java/com/example/scheduleappdev/user/repository/UserRepository.java
+++ src/main/java/com/example/scheduleappdev/user/repository/UserRepository.java
@@ -6,5 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    /**
+     * 유저명으로 유저 검색
+     *
+     * @param user 유저명 입력
+     * @return Optional<User>로 검색 결과 반환
+     */
     Optional<User> findByUsername(String user);
 }
