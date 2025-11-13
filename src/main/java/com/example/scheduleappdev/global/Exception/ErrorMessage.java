aa3c5098343ec368a787d5e473dace8ce2997b16
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-11-13 18:18:02 +0900

    feat: 커스텀 에러 관리 Enum
    - NOT_LOGGED_IN : 비로그인 상태로 접근 시 발생
    - INCORRECT_PASSWORD : 비밀번호 불일치 시 발생
    - NOT_FOUND_USER : 존재하지 않는 User 선택 시 발생
    - UNMATCHED_USER : 로그인된 유저와 수정/삭제하려는 객체의 유저 불일치 시 발생
    - NOT_FOUND_TODO : 존재하지 않는 Todo 선택 시 발생

--- src/main/java/com/example/scheduleappdev/global/Exception/ErrorMessage.java
+++ src/main/java/com/example/scheduleappdev/global/Exception/ErrorMessage.java
@@ -10,7 +10,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorMessage {
-    INCORRECT_PASSWORD(HttpStatus.UNAUTHORIZED,"비밀번호가 일치하지 않습니다.");
+    NOT_LOGGED_IN(HttpStatus.UNAUTHORIZED, "로그인 되지 않았습니다."),
+    INCORRECT_PASSWORD(HttpStatus.UNAUTHORIZED,"비밀번호가 일치하지 않습니다."),
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "해당 유저가 존재하지 않습니다."),
+    UNMATCHED_USER(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
+    NOT_FOUND_TODO(HttpStatus.NOT_FOUND, "해당 일정이 존재하지 않습니다.");
 
     private final HttpStatus status;
     private final String message;
