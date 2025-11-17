be6f6639f30aa9a2406bac64fdf576f1707a2da3
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-11-17 19:19:49 +0900

    feat: NOT_FOUND_COMMENT 커스텀 에러 추가
    - 존재하지 않는 댓글 선택 시 발생

--- src/main/java/com/example/scheduleappdev/global/Exception/ErrorMessage.java
+++ src/main/java/com/example/scheduleappdev/global/Exception/ErrorMessage.java
@@ -14,7 +14,8 @@ public enum ErrorMessage {
     INCORRECT_PASSWORD(HttpStatus.UNAUTHORIZED,"비밀번호가 일치하지 않습니다."),
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, "해당 유저가 존재하지 않습니다."),
     UNMATCHED_USER(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
-    NOT_FOUND_TODO(HttpStatus.NOT_FOUND, "해당 일정이 존재하지 않습니다.");
+    NOT_FOUND_TODO(HttpStatus.NOT_FOUND, "해당 일정이 존재하지 않습니다."),
+    NOT_FOUND_COMMENT(HttpStatus.NOT_FOUND, "해당 댓글이 존재하지 않습니다.");
 
     private final HttpStatus status;
     private final String message;
