07bfee241f9454981897d08eb5198b197be241fb
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-11-17 17:37:17 +0900

    feat: 댓글 생성 API 구현
    - request DTO : 댓글
    - response DTO : 일정id, 댓글id, 댓글, 작성자, 생성일, 수정일
    - Controller : 세션키, 일정ID, request 입력 받아 Service 전달 후, 생성 성공 시 201 CREATED 상태코드와 생성된 내용 반환
    - Service : 세션키의 유저ID와 일정ID, request로 댓글 생성 후 리포지토리에 저장, DTO에 생성된 내용 담아 반환

--- src/main/java/com/example/scheduleappdev/comment/controller/CommentController.java
+++ src/main/java/com/example/scheduleappdev/comment/controller/CommentController.java
@@ -1,11 +1,37 @@
 package com.example.scheduleappdev.comment.controller;
 
+import com.example.scheduleappdev.comment.dto.*;
 import com.example.scheduleappdev.comment.service.CommentService;
+import com.example.scheduleappdev.global.Exception.ErrorMessage;
+import com.example.scheduleappdev.global.Exception.TodoServiceException;
+import com.example.scheduleappdev.user.dto.UserForHttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
+
+    /**
+     * 댓글 생성하기
+     *
+     * @param sessionUser loginUser 이름을 가진 세션 속성 찾아 DTO에 주입
+     * @param todoId API Path로 일정 ID 선택받기
+     * @param request HTTP Body로 내용 받기
+     * @return 201 CREATED 상태코드와 생성된 내용 반환
+     * @throws TodoServiceException 비로그인 상태로 접근 시 Not_Logged_In 예외 발생
+     */
+    @PostMapping("/todos/{todoId}/comments")
+    public ResponseEntity<CreateCommentResponse> createComment(
+            @SessionAttribute(name = "loginUser", required = false) UserForHttpSession sessionUser,
+            @PathVariable Long todoId,
+            @Valid @RequestBody CreateCommentRequest request) {
+        if (sessionUser == null) { throw new TodoServiceException(ErrorMessage.NOT_LOGGED_IN); }
+        CreateCommentResponse result = commentService.create(todoId, request, sessionUser.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
 }
--- src/main/java/com/example/scheduleappdev/comment/service/CommentService.java
+++ src/main/java/com/example/scheduleappdev/comment/service/CommentService.java
@@ -1,11 +1,47 @@
 package com.example.scheduleappdev.comment.service;
 
+import com.example.scheduleappdev.comment.dto.*;
+import com.example.scheduleappdev.comment.entity.Comment;
 import com.example.scheduleappdev.comment.repository.CommentRepository;
+import com.example.scheduleappdev.global.Exception.ErrorMessage;
+import com.example.scheduleappdev.global.Exception.TodoServiceException;
+import com.example.scheduleappdev.todo.entity.Todo;
+import com.example.scheduleappdev.todo.repository.TodoRepository;
+import com.example.scheduleappdev.user.entity.User;
+import com.example.scheduleappdev.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final TodoRepository todoRepository;
+
+    /**
+     * 댓글 생성하기
+     *
+     * @param todoId 일정 ID 받기
+     * @param request 내용 받기(댓글)
+     * @param userId Http Session을 통해 유저ID 받기
+     * @return 생성된 내용 DTO에 담아 반환
+     * @throws TodoServiceException 존재하지 않는 유저로 접근 시 Not_Found_User 예외 발생
+     * @throws TodoServiceException 존재하지 않는 일정ID 입력 시 Not_Found_Todo 예외 발생
+     */
+    @Transactional
+    public CreateCommentResponse create(Long todoId, CreateCommentRequest request, Long userId) {
+        User creator = userRepository.findById(userId)
+                .orElseThrow(() -> new TodoServiceException(ErrorMessage.NOT_FOUND_USER));
+        Todo todo = todoRepository.findById(todoId)
+                .orElseThrow(() -> new TodoServiceException(ErrorMessage.NOT_FOUND_TODO));
+        Comment comment = new Comment(
+                request.getComment(), creator, todo
+        );
+        Comment savedComment = commentRepository.save(comment);
+        return new CreateCommentResponse(
+                savedComment.getTodo().getId(), savedComment.getId(), savedComment.getComment(), savedComment.getCreator().getUsername(), savedComment.getCreatedAt(), savedComment.getModifiedAt()
+        );
+    }
 }
