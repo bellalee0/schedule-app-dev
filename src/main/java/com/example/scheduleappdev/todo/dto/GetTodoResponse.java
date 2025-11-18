0d17b9236717fa130da18099ffef66b09c0e2125
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-11-18 16:07:11 +0900

    feat: 일정 조회 응답 DTO에 댓글 수 필드 추가

--- src/main/java/com/example/scheduleappdev/todo/dto/GetTodoResponse.java
+++ src/main/java/com/example/scheduleappdev/todo/dto/GetTodoResponse.java
@@ -5,21 +5,23 @@ import lombok.Getter;
 import java.time.LocalDateTime;
 
 /**
- * 일정 조회 응답 DTO (일정id, 제목, 내용, 작성자, 생성일, 수정일)
+ * 일정 조회 응답 DTO (일정id, 제목, 내용, 댓글 수, 작성자, 생성일, 수정일)
  */
 @Getter
 public class GetTodoResponse {
     private final Long id;
     private final String title;
     private final String contents;
+    private final int numberOfComments;
     private final String creator;
     private final LocalDateTime createdAt;
     private final LocalDateTime modifiedAt;
 
-    public GetTodoResponse(Long id, String title, String contents, String creator, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+    public GetTodoResponse(Long id, String title, String contents, int numberOfComments, String creator, LocalDateTime createdAt, LocalDateTime modifiedAt) {
         this.id = id;
         this.title = title;
         this.contents = contents;
+        this.numberOfComments = numberOfComments;
         this.creator = creator;
         this.createdAt = createdAt;
         this.modifiedAt = modifiedAt;
